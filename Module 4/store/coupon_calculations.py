508c4da44997f4576b18a4f73943ef92fef8e9af
Dylan Thomas <djthomas4@dmacc.edu>
2020-09-21 00:07:49 -0500

    Second commit coupon calc first tests pass

--- Module 4/store/coupon_calculations.py
+++ Module 4/store/coupon_calculations.py
@@ -1,6 +1,27 @@
-def calculate_price(price, cash_coupon, percent_coupon):
-    pass
+def calculate_price(originalPrice, cash_coupon, percent_coupon):
+    #setup
+    if originalPrice < 10:
+        shippingRate = 5.95
+    elif 10 <= originalPrice <30:
+        shippingRate = 7.95
+    elif 30 <= originalPrice <50:
+        shippingRate = 11.95
+    elif originalPrice >= 50:
+        shippingRate = 0
+    taxRate = .06
+    percentActual = 1 - percent_coupon
+
+    #math
+    midPrice = (originalPrice - cash_coupon) * percentActual
+    taxCost = midPrice * taxRate
+    endPrice = midPrice + taxCost + shippingRate
+
+    return endPrice
 
 
 if __name__ == '__main__':
-
+    originalPrice = float(input("Enter the original price: "))
+    cash_coupon = float(input("Enter the cash coupon you have: "))
+    percent_coupon = float(input("Enter the percent off coupon amount: "))
+    endPrice = calculate_price(originalPrice, cash_coupon, percent_coupon)
+    print(endPrice)
--- Module 4/test/test_coupon_calculations.py
+++ Module 4/test/test_coupon_calculations.py
@@ -5,12 +5,12 @@ from coupon_calculations import calculate_price
 class MyTestCase(unittest.TestCase):
     def test_price_under_ten(self):
         #(example) self.assertAlmostEqual(calculate_price(cost, cash off, percent off),"expected result here", places=4)
-        self.assertAlmostEqual(calculate_price(4.0, 5.0, .1), 5.95, places=4)
-        self.assertAlmostEqual(calculate_price(6.0, 5.0, .15), 6.851, places=4)
-        self.assertAlmostEqual(calculate_price(8.24, 5.0, .2), 9.838, places=4)
-        self.assertAlmostEqual(calculate_price(6.45, 10.0, .1), 2.045, places=4)
-        self.assertAlmostEqual(calculate_price(7.85, 10.0, .15), 3.4775, places=4)
-        self.assertAlmostEqual(calculate_price(9.24, 10.0, .2), 5.038, places=4)
+        self.assertAlmostEqual(calculate_price(4.0, 5.0, 0.1), 4.996, places=2)
+        self.assertAlmostEqual(calculate_price(6.0, 5.0, 0.15), 6.851, places=2)
+        self.assertAlmostEqual(calculate_price(8.24, 5.0, 0.2), 8.697, places=2)
+        self.assertAlmostEqual(calculate_price(6.45, 10.0, 0.1), 2.563, places=2)
+        self.assertAlmostEqual(calculate_price(7.85, 10.0, 0.15), 4.013, places=2)
+        self.assertAlmostEqual(calculate_price(9.24, 10.0, 0.2), 5.306, places=2)
 
 
 
