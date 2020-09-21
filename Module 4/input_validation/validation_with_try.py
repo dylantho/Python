9a961c9d99f2aefe9318380a807adf26278964fc
Dylan Thomas <djthomas4@dmacc.edu>
2020-09-21 02:07:52 -0500

    Fourth commit input validation pass

--- Module 4/input_validation/validation_with_try.py
+++ Module 4/input_validation/validation_with_try.py
@@ -3,26 +3,43 @@ Program: validation_with_try.py
 Author: Dylan Thomas
 Last date modified: 09/21/2020
 """
-
+import sys
 
 def average(score1, score2, score3):
     if score1 < 0:
         raise ValueError
     elif score2 < 0:
         raise ValueError
-    elif score3 <0:
+    elif score3 < 0:
         raise ValueError
 
     return ((score1+score2+score3)/3)
 
+first_name = input('Enter your first name: ')
+last_name = input('Enter your last name: ')
+age = input('Enter your age: ')
 
 def main():
-    first_name = input('Enter your first name: ')
-    last_name = input('Enter your last name: ')
-    age = input('Enter your age: ')
-    score1 = int(input('Enter your first score: '))
-    score2 = int(input('Enter your second score: '))
-    score3 = int(input('Enter your third score: '))
+    try:
+        score1 = int(input('Enter your first score: '))
+        if score1 < 0:
+            raise ValueError
+
+        score2 = int(input('Enter your second score: '))
+        if score2 < 0:
+            raise ValueError
+
+        score3 = int(input('Enter your third score: '))
+        if score3 < 0:
+            raise ValueError
+
+
+    except ValueError:
+        print()
+        print("Error: Please input a positive number next time")
+        print()
+        main()
+
     average_scores = average(score1, score2, score3)
 
     print(f"{last_name}, {first_name} Age: {age} Average score: {average_scores} ")
