40da3fde6256e87993d4fdceafec72d3e3e3f6f5
Dylan Thomas <djthomas4@dmacc.edu>
2020-09-21 01:21:39 -0500

    Second commit input validation fail

--- Module 4/test/test_validation_with_try.py
+++ Module 4/test/test_validation_with_try.py
@@ -4,7 +4,7 @@ from validation_with_try import average
 class MyTestCase(unittest.TestCase):
     def test_average_negative_input(self):
         with self.assertRaises(ValueError):
-            average(-90, 89, 78)
+            average(90, -89, 78)
 
 if __name__ == '__main__':
     unittest.main()
