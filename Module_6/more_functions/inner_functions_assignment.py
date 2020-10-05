732f2a89b2e59033d684a78ec9b7b1171b268dd5
Dylan Thomas <djthomas4@dmacc.edu>
2020-10-05 01:08:42 -0500

    Rectangle test passes, square passes

--- Module_6/more_functions/inner_functions_assignment.py
+++ Module_6/more_functions/inner_functions_assignment.py
@@ -7,20 +7,30 @@ Last date modified: 10/05/2020
 
 
 def measurements(a_list):
+    width = 0
     length = a_list[0]
-    width = a_list[1]
+    if len(a_list) == 2:
+        width = a_list[1]
 
 
     def area(a_list):
         areaCalc = 0
-        areaCalc = length * width
-
+        if len(a_list) == 1:
+            areaCalc = length * length
+        elif len(a_list) == 2:
+            areaCalc = length * width
+        else:
+            print("There should only be one or two values.")
         return areaCalc
 
     def perimeter(a_list):
         perimeterCalc = 0
-        perimeterCalc = (length * 2) + (width * 2)
-
+        if len(a_list) == 1:
+            perimeterCalc = length * 4
+        elif len(a_list) == 2:
+            perimeterCalc = (length * 2) + (width * 2)
+        else:
+            print("There should only be one or two values.")
         return perimeterCalc
 
     listPerimeter = str(perimeter(a_list))
