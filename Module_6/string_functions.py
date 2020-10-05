0ddb4dc341258c2bc9619fe85674dd569bd157ef
Dylan Thomas <djthomas4@dmacc.edu>
2020-10-04 20:31:23 -0500

    Test string functions second commit (Passed)

--- Module_6/string_functions.py
+++ Module_6/string_functions.py
@@ -5,7 +5,15 @@ Last date modified: 10/04/2020
 """
 
 def multiply_string(message, n):
-    pass
+    """This function takes a string and prints it to number or times specified by the user.
+    :param message, any string
+    :param n, a positive integer
+    :returns a new string of the original string printed n times in a row
+    """
+    for i in range(n):
+        new_message = (message * n)
+
+    return new_message
 
 if __name__ == '__main__':
     pass
