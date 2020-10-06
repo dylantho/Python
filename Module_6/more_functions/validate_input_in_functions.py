7d6328b1f365f5be3e00e1239e8570c387cdcb27
Dylan Thomas <djthomas4@dmacc.edu>
2020-10-05 20:52:45 -0500

    Non-numeric and invalid message tests pass.

--- Module_6/more_functions/validate_input_in_functions.py
+++ Module_6/more_functions/validate_input_in_functions.py
@@ -12,6 +12,7 @@ def score_input(test_name, test_score=0, invalid_message='Invalid test score, tr
     :param invalid_message, the message that is displayed when the user enters an invalid input
     :returns The name of the test and score
     """
+
     while True:
         try:
             while (type(test_score) != int) or (test_score < 0) or (test_score > 100):
@@ -23,13 +24,10 @@ def score_input(test_name, test_score=0, invalid_message='Invalid test score, tr
         except ValueError:
             raise ValueError
 
-
     test_score = str(test_score)
 
     # return { test_name: test_score}
     return test_name + ": " + test_score
-
-
 
 
 if __name__ == '__main__':
