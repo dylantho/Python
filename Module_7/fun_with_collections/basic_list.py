83fa58b7aa4cbd6967ac0bc12a618da0a0e7f6e5
Dylan Thomas <djthomas4@dmacc.edu>
2020-10-12 02:28:22 -0500

    test make_list() passes

--- Module_7/fun_with_collections/basic_list.py
+++ Module_7/fun_with_collections/basic_list.py
@@ -4,9 +4,26 @@ Author: Dylan Thomas
 Last date modified: 10/12/2020
 """
 
-def make_list():
-    pass
-
-
 def get_input():
     pass
+
+def make_list():
+    myList = []
+
+    for x in range(3):
+        userInput = get_input()
+
+        try:
+            userInput = int(userInput)
+        except ValueError:
+            print("non numeric")
+
+
+        myList.insert(len(myList), userInput)
+
+        #myList.append(userInput) Alternate method
+
+
+    return myList
+
+
