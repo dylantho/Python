1496cde6a51a0783a9317d768d457ef59687bf66
Dylan Thomas <djthomas4@dmacc.edu>
2020-10-17 22:07:11 -0500

    Test_A passes

--- Module_8/more_fun_with_collections/assign_average.py
+++ Module_8/more_fun_with_collections/assign_average.py
@@ -6,4 +6,13 @@ Last date modified: 10/17/2020
 
 
 def switch_average(argument):
-    pass
+
+    def test_a():
+        return 97
+
+    switch_dictionary = {
+        'A': test_a,
+    }
+
+    function = switch_dictionary.get(argument, lambda: "Invalid key")
+    return function()
