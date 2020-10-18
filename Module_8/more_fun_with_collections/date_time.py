278ed43b57492ba10c6b1f02241ff94792c6002a
Dylan Thomas <djthomas4@dmacc.edu>
2020-10-17 22:49:44 -0500

    Test for half_birthday passes

--- Module_8/more_fun_with_collections/date_time.py
+++ Module_8/more_fun_with_collections/date_time.py
@@ -5,8 +5,14 @@ Last date modified: 10/17/2020
 """
 
 
+from datetime import datetime, timedelta
+
+
 def half_birthday(birthday):
-    pass
+    next_half_bday = birthday + timedelta(days=182)
+    print('Next half birthday:', str(next_half_bday))
+
+    return str(next_half_bday)
 
 
 if __name__ == '__main__':
