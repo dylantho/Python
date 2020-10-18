fe5f7b62941a4db307c299d21f5970bd359f584b
Dylan Thomas <djthomas4@dmacc.edu>
2020-10-17 19:43:00 -0500

    in_dict written and test_dict_membership tests pass

--- Module_8/more_fun_with_collections/dict_membership.py
+++ Module_8/more_fun_with_collections/dict_membership.py
@@ -4,8 +4,12 @@ Author: Dylan Thomas
 Last date modified: 10/17/2020
 """
 
+
 def in_dict(user_dict, item):
-    pass
+    if item in user_dict:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
