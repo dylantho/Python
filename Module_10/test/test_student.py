3fae268433e5805579b2418197c18e99cb7b8586
Dylan Thomas <djthomas4@dmacc.edu>
2020-11-02 18:33:39 -0600

    Second unit test (All attributes) passes for student test.

--- Module_10/test/test_student.py
+++ Module_10/test/test_student.py
@@ -3,16 +3,30 @@ from Student import Student
 
 
 class MyTestCase(unittest.TestCase):
+    # Test 1: Required parameters Status: Pass
     def setUp(self):
         self.student = Student('Oliver', 'John', 'Media')
-
-    def tearDown(self):
-        del self.student
 
     def test_object_created_required_attributes(self):
         self.assertEqual(self.student.last_name, 'Oliver')
         self.assertEqual(self.student.first_name, 'John')
         self.assertEqual(self.student.major, 'Media')
+
+    def tearDown(self):
+        del self.student
+
+    # Test 2: Required parameters Status: Pass
+    def setUp(self):
+        self.student = Student('Oliver', 'John', 'Media', 3.8)
+
+    def test_object_created_all_attributes(self):
+        self.assertEqual(self.student.last_name, 'Oliver')
+        self.assertEqual(self.student.first_name, 'John')
+        self.assertEqual(self.student.major, 'Media')
+        self.assertEqual(self.student.gpa, 3.8)
+
+    def tearDown(self):
+        del self.student
 
 
 if __name__ == '__main__':
